209df7fd6c873102e0928e5be1b9ce4df5b42fc0
plumgit15 <63837262+plumgit15@users.noreply.github.com>
2021-10-17 10:35:54 +0900

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import ProductPage from "./components/pages/ProductPage";
+import HomePage from "./components/pages/HomePage";
+
+export const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/products" component={ProductPage} exact />
+        <Route path="/" component={HomePage} exact />
+      </Switch>
+    </Router>
+  );
+};
